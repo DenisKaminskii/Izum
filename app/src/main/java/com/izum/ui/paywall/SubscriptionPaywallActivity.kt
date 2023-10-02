675af1851fde3dcbe4103a75882a5e7a67af0fea
Denis Kaminskii <d.kaminsky@prisma-ai.com>
2023-10-02 19:10:26 +0300

    Add paywall (empty)

--- app/src/main/java/com/izum/ui/paywall/SubscriptionPaywallActivity.kt
+++ app/src/main/java/com/izum/ui/paywall/SubscriptionPaywallActivity.kt
@@ -1,9 +1,28 @@
 package com.izum.ui.paywall
 
+import android.animation.ValueAnimator
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.izum.R
 import com.izum.databinding.ActivitySubscriptionPaywallBinding
 import com.izum.ui.BaseActivity
+import com.izum.ui.dpF
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
+
+private enum class Price {
+    WEEK,
+    MONTH,
+    LIFETIME
+}
 
 @AndroidEntryPoint
 class SubscriptionPaywallActivity : BaseActivity() {
@@ -19,8 +38,47 @@ class SubscriptionPaywallActivity : BaseActivity() {
 
     override fun initView(args: Bundle) = with(binding) {
         super.initView(args)
+
+        iPriceFirst.setOnClickListener { update(Price.WEEK) }
+        iPriceSecond.setOnClickListener { update(Price.MONTH) }
+        iPriceThird.setOnClickListener { update(Price.LIFETIME) }
+
+        update(Price.MONTH)
     }
 
+    private fun update(price: Price) = with(binding) {
+        iPriceFirst.update(
+            PremiumPriceItemState(
+                title = "Week",
+                price = "5$",
+                subtitle = "per week",
+                isSelected = price == Price.WEEK,
+                primaryColor = getColor(R.color.paywall_red),
+                textColor = getColor(R.color.paywall_black)
+            )
+        )
 
+        iPriceSecond.update(
+            PremiumPriceItemState(
+                title = "Month",
+                price = "20$",
+                subtitle = "per month",
+                isSelected = price == Price.MONTH,
+                primaryColor = getColor(R.color.paywall_purple),
+                textColor = getColor(R.color.paywall_black)
+            )
+        )
+
+        iPriceThird.update(
+            PremiumPriceItemState(
+                title = "LifeTime",
+                price = "42$",
+                subtitle = "forever",
+                isSelected = price == Price.LIFETIME,
+                primaryColor = getColor(R.color.paywall_green),
+                textColor = getColor(R.color.paywall_black)
+            )
+        )
+    }
 
 }
